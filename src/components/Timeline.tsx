08b8ce7c995913509901de74f97fd33e3e06ca2a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-02-22 19:35:25 +0530

    Implemented timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,21 @@
 import styles from './Timeline.module.scss';
 
+const items = [
+  { active: true, label: '1' },
+  { active: true, label: '2' },
+  { active: false, label: '3' },
+  { active: false, label: '4' },
+];
+
 const Timeline = () => {
   return (
     <div className={styles.timeline}>
-      <span></span> <span></span> <span></span> <span></span>
+      <div className={styles.line}></div>
+      {items.map((item) => (
+        <span className={item.active ? styles.active : ''} key={item.label}>
+          {item.label}
+        </span>
+      ))}
     </div>
   );
 };
