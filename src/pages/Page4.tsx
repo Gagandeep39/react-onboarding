295bf01a69c2681c04b9e68d0dee2b18343e1ead
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2022-02-23 18:02:51 +0530

    Styling iproveents

--- src/pages/Page4.tsx
+++ src/pages/Page4.tsx
@@ -1,8 +1,12 @@
 import SummaryText from '../components/SummaryText';
+import styles from './Page4.module.scss';
 
 const Page4 = () => {
   return (
     <div>
+      <span className={styles.completed}>
+        <span className={styles.check}></span>
+      </span>
       <SummaryText
         heading="Congratulations, Eren!"
         subheading="You have completed onboarding, you can start using Eden!"
